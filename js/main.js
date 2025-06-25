deab3812bcbec3dee0a4180f8b546a1c3106075d
Aki-i41 <a.izm8@chiba-c.ed.jp>
2025-06-25 14:57:14 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -96,8 +96,8 @@ function onTouchMove(event) {
     var deltaX = newX - touchX;
     var deltaY = newY - touchY;
     
-    lon += deltaX * 3;
-    lat -= deltaY * 3;
+    lon += deltaX * 10;
+    lat -= deltaY * 10;
     lat = Math.max(-85, Math.min(85, lat));
     
     touchX = newX;
